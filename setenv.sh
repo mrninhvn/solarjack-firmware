07f5fc9aa7f1da1b2f9e45b93d564a1dd90157a4
mrninhvn <mr.ninh.vn@outlook.com>
2021-07-29 15:21:45 +0700

    edit SDK root

--- setenv.sh
+++ setenv.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-export GCC_ARM=/opt/gcc-arm-none-eabi-8-2019-q3-update 
-export NRF_SDK_KBD=~/dev/nrf-sdk/nRF5_SDK_15.3.0
-export NRF_SDK_RCVR=~/dev/nrf-sdk/nRF5_SDK_12.3.0
-export NRF_TOOLS=/opt/nrfjprog
-export SEGGER=/opt/SEGGER
+# export GCC_ARM=/opt/gcc-arm-none-eabi-8-2019-q3-update 
+# export NRF_SDK_KBD=~/dev/nrf-sdk/nRF5_SDK_15.3.0
+# export NRF_SDK_RCVR=~/dev/nrf-sdk/nRF5_SDK_12.3.0
+# export NRF_TOOLS=/opt/nrfjprog
+# export SEGGER=/opt/SEGGER
 
+export NRF_SDK_RCVR=/mnt/d/Keyboard/nRF5_SDK_12.3.0_d7731ad
+export NRF_SDK_KBD=/mnt/d/Keyboard/nRF5_SDK_15.3.0_59ac345
